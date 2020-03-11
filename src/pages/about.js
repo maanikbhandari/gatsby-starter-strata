a49ccd94e2375b49bf232192d06659a890d0bf78
maanikbhandari <maanik94@hotmail.com>
2020-03-11 13:18:34 -0600

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,65 @@
 
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Layout from '../components/layout'
+// import Lightbox from 'react-images'
+import Gallery from '../components/Gallery'
+
+import thumb01 from '../assets/images/thumbs/01.jpg'
+import thumb02 from '../assets/images/thumbs/02.jpg'
+import thumb03 from '../assets/images/thumbs/03.jpg'
+import thumb04 from '../assets/images/thumbs/04.jpg'
+import thumb05 from '../assets/images/thumbs/05.jpg'
+import thumb06 from '../assets/images/thumbs/06.jpg'
+
+import full01 from '../assets/images/fulls/01.jpg'
+import full02 from '../assets/images/fulls/02.jpg'
+import full03 from '../assets/images/fulls/03.jpg'
+import full04 from '../assets/images/fulls/04.jpg'
+import full05 from '../assets/images/fulls/05.jpg'
+import full06 from '../assets/images/fulls/06.jpg'
+
+const DEFAULT_IMAGES = [
+    { id: '1', source: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '2', source: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '3', source: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '4', source: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '5', source: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '6', source: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+];
+
+class HomeIndex extends React.Component {
+
+    render() {
+        const siteTitle = "Gatsby Starter - Strata"
+        const siteDescription = "Site description"
+
+        return (
+            <Layout>
+                <Helmet>
+                        <title>{siteTitle}</title>
+                        <meta name="description" content={siteDescription} />
+                </Helmet>
+
+                <div id="main">
+
+                    <section id="one">
+                        <header className="major">
+                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
+                            magna sed accumsan arcu neque.</h2>
+                        </header>
+                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+                        <ul className="actions">
+                            <li><a href="#" className="button">Learn More</a></li>
+                        </ul>
+                    </section>
+
+                </div>
+
+            </Layout>
+        )
+    }
+}
+
+export default HomeIndex
